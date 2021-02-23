5978c3afc659a1f09af9ad6ce47bc6ae341769fd
am-MongoDB <andrew.morgan@mongodb.com>
2021-02-23 09:36:19 +0000

    Bootstrap iOS app

---
+++ iOS/ProrcrastDonate/Pods/Target Support Files/Pods-ProrcrastDonate-ProrcrastDonateUITests/Pods-ProrcrastDonate-ProrcrastDonateUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProrcrastDonate_ProrcrastDonateUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProrcrastDonate_ProrcrastDonateUITests
+@end
---
+++ iOS/ProrcrastDonate/ProrcrastDonate/ProrcrastDonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProrcrastDonateApp.swift
+//  ProrcrastDonate
+//
+//  Created by Andrew Morgan on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProrcrastDonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
